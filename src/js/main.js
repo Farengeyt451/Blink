c2e115ef406301d14c6fa8fe6b369c032ee0836f
Alexander <strannik11.92@gmail.com>
2016-12-11 17:53:16 +0300

    Add button to start animation, js blink

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,44 @@
-console.log("Test");
+// Определение ширины дисплея
+var x = screen.width;
 
-var seconds = 5;
-var funcTimer = function() {
+// Добавление контейнера (100% вьюпорта) для окружности
+var funcLinearCont = function() {
+	document.getElementById("anim-main").classList.add("linear-cont");
+};
+
+// Добавление окружности
+var funcMainCircle = function() {
+	document.getElementById("anim-circle").classList.add("main-circle");
+	document.getElementById("anim-circle").classList.add("circle-blinking");
+};
+
+// Скрытие контейнера таймера
+var funcHideTimer = function() {
+	document.getElementById("timer-cont").classList.add("main-timer-hide");
+};
+
+// // Запуск анимации движения окружности (верх - низ)
+// var funcTopBot = function() {
+// 	document.getElementById("anim-main").classList.add("circle-top-bot");
+// };
+// setTimeout(funcTopBot, 5000);
+
+// // Запуск анимации движения окружности (лево - право)
+// var funcLeftRight = function() {
+// 	document.getElementById("anim-main").classList.remove("circle-top-bot");
+// 	document.getElementById("anim-main").classList.add("circle-left-right");
+// };
+
+// if(x < 1500) {
+// 	setTimeout(funcLeftRight, 10000);
+// 	console.log("X < 1500");
+// };
+
+// Запуск таймера
+startanim.onclick = function() {
+	// Таймер, задержка перед началом анимации
+	var seconds = 5;
+	var funcTimer = function() {
 	// if(seconds < 9) {
 	// 	seconds = "0" + seconds;
 	// }
@@ -11,16 +48,11 @@ var funcTimer = function() {
 	if(seconds == 0) {
 		return false;
 	}
-	if(seconds == 0) {
-		
-	}
 	seconds--;
 	setTimeout(funcTimer, 1000);
+	};
+	funcTimer();
+	funcLinearCont();
+	funcMainCircle();
+	setTimeout(funcHideTimer, 5000);
 };
-
-funcTimer();
-
-var funcTopBot = function() {
-	document.getElementById("anim-main").classList.add("circle-top-bot");
-};
-setTimeout(funcTopBot, 5000);
