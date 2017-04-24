14af437c6fea6c026c22f9f657bf46eb1583923e
Alexander <strannik11.92@gmail.com>
2017-04-24 17:26:35 +0300

    Add bew nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,23 @@
 // JS код для главного меню
 // ------------------------
 
-jQuery(document).ready(function($){
-	// Открыть меню
-	$(".cd-menu-trigger").on("click", function(event){
-		event.preventDefault();
-		$("#cd-main-content").addClass("move-out");
-		$("#main-nav").addClass("is-visible");
-		$(".cd-shadow-layer").addClass("is-visible");
-	});
-	// Закрыть меню
-	$(".cd-close-menu").on("click", function(event){
-		event.preventDefault();
-		$("#cd-main-content").removeClass("move-out");
-		$("#main-nav").removeClass("is-visible");
-		$(".cd-shadow-layer").removeClass("is-visible");
-	});
+jQuery(document).ready(function(){
+	if( $('.cd-stretchy-nav').length > 0 ) {
+		var stretchyNavs = $('.cd-stretchy-nav');
+		
+		stretchyNavs.each(function(){
+			var stretchyNav = $(this),
+				stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+			
+			stretchyNavTrigger.on('click', function(event){
+				event.preventDefault();
+				stretchyNav.toggleClass('nav-is-visible');
+			});
+		});
 
-	// Блюр эффект
-	set_clip_property();
-	$(window).on("resize", function(){
-		set_clip_property();
-	});
-
-	function set_clip_property() {
-		var $header_height = $(".cd-header").height(),
-			$window_height = $(window).height(),
-			$header_top = $window_height - $header_height,
-			$window_width = $(window).width();
-		$(".cd-blurred-bg").css("clip", "rect("+$header_top+"px, "+$window_width+"px, "+$window_height+"px, 0px)");
+		$(document).on('click', function(event){
+			( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+		});
 	}
 });
 // --------------------------------------------------
