80abbf091f408b3752a3ffb85c601c92b418667b
Alexander <strannik11.92@gmail.com>
2017-06-18 16:03:58 +0300

    Add donation modal window

--- src/js/partials/main-menu.js
+++ src/js/partials/main-menu.js
@@ -35,28 +35,18 @@ var closeContacts = function() {
 	document.getElementById("contacts").classList.remove("contacts-show");
 };
 
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-		modal.style.display = "block";
+// Вызов модального окна доната
+var modalDonate = document.getElementById("myModal");
+var btnDonate = document.getElementById("myBtn");
+var spanDonate = document.getElementsByClassName("close")[0];
+btnDonate.onclick = function() {
+		modalDonate.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-		modal.style.display = "none";
+spanDonate.onclick = function() {
+		modalDonate.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-		if (event.target == modal) {
-				modal.style.display = "none";
+		if (event.target == modalDonate) {
+				modalDonate.style.display = "none";
 		}
 };
