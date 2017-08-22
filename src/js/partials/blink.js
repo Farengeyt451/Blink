ca805f191505332b085b1745857447595955a908
Alexander <strannik11.92@gmail.com>
2017-08-22 14:02:47 +0300

    Add simple timer

--- src/js/partials/blink.js
+++ src/js/partials/blink.js
@@ -376,3 +376,15 @@ if(elGradient) {
 	// Start
 	window.requestAnimationFrame(updateGradient);
 }
+
+var count=20;
+var counter = setInterval(timer, 1000);
+function timer() {
+	count=count-1;
+	if (count <= 0)
+	{
+		clearInterval(counter);
+		return;
+	}
+document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
+}
