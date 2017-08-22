32a7d190f78a8288795b22bfd1cf28eba7a95144
Alexander <strannik11.92@gmail.com>
2017-08-22 10:23:24 +0300

    Change close method for main navigation

--- src/js/partials/main-menu.js
+++ src/js/partials/main-menu.js
@@ -1,56 +1,66 @@
-// ------------------------
-// JS код для главного меню
-// ------------------------
+//-------------------------
+//-JS код для главного меню
+//-------------------------
+var elNav = document.getElementById("main-nav");
+var navClose = document.getElementsByClassName("cd-nav-trigger")[0];
+var elNavCont = document.getElementById("showContacts");
+var modalDonate = document.getElementById("myModal");
+var btnDonate = document.getElementById("myBtn");
+var spanDonate = document.getElementsByClassName("close")[0];
 
+// Переключение классов
 function toggleClass(element, className){
-		if (!element || !className){
-				return;
-		}
-		var classString = element.className, nameIndex = classString.indexOf(className);
-		if (nameIndex == -1) {
-				classString += " " + className;
-		}
-		else {
-				classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-		}
-		element.className = classString;
+	if (!element || !className){
+		return;
+	}
+	var classString = element.className, nameIndex = classString.indexOf(className);
+	if (nameIndex === -1) {
+		classString += " " + className;
+	}
+	else {
+		classString = classString.substr(0, nameIndex) + classString.substr(nameIndex + className.length);
+	}
+	element.className = classString;
 }
 
-var elNav = document.getElementById("main-nav");
+// Раскрытие/закрытие меню навигации
 if(elNav){
 	document.getElementById("main-nav").addEventListener("click", function() {
 		toggleClass(document.getElementById("main-nav"), "nav-is-visible");
 	});
+
 }
 
-// Показ блока контактов
-var elNavCont = document.getElementById("showContacts");
+// Раскрытие блока контактов
 if(elNavCont){
 	document.getElementById("showContacts").addEventListener("click", function() {
 		toggleClass(document.getElementById("contacts"), "contacts-show");
 	});
 }
 
-var closeContacts = function() {
+// Закрытие блока контактов
+closeContacts = function() {
 	document.getElementById("contacts").classList.remove("contacts-show");
 };
 
 // Вызов модального окна доната
-var modalDonate = document.getElementById("myModal");
-var btnDonate = document.getElementById("myBtn");
-var spanDonate = document.getElementsByClassName("close")[0];
 if(btnDonate){
 	btnDonate.onclick = function() {
 	modalDonate.style.display = "block";
 	};
 }
+
+// Закрытие окна доната и меню навигации
+window.onclick = function(event) {
+		if (event.target == modalDonate) {
+			modalDonate.style.display = "none";
+		}
+		if (event.target !== navClose) {
+			document.getElementById("main-nav").classList.remove("nav-is-visible");
+		}
+};
 if(spanDonate){
 	spanDonate.onclick = function() {
 		modalDonate.style.display = "none";
 	};
 }
-window.onclick = function(event) {
-		if (event.target == modalDonate) {
-				modalDonate.style.display = "none";
-		}
-};
